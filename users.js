28a1935a38dd37a2477be1b25443b6331a63ffe3
Sharon Frenkel <shrfrn@gmail.com>
2026-01-08 19:33:37 +0200

    Add auto-gen scenes and users

--- users.js
+++ users.js
@@ -1,13 +1,16 @@
 // Home Assistant user IDs
-// To find user IDs: ssh root@homeassistant.local "ha core exec -c 'cat /config/.storage/auth'" | grep -A2 '"name"'
+// Auto-generated from inventory - DO NOT edit the USERS block
+// Add custom groups below the auto-generated section
 
 export const USERS = {
   sharon: 'bdc857a611d74e9998fdc8535024b263',
   shahar: 'b10953f68ec043cea57260ca2f5e0880',
   tom: '42aa497a257c46f9a1c8650a32a46e57',
+  amai: '18b8209ec7814a4bb6b69b40bac90270',
 }
 
 // Pre-defined groups for convenience
 export const PARENTS = [USERS.sharon, USERS.shahar]
+export const KIDS = [USERS.tom, USERS.amai]
 export const ALL = Object.values(USERS)
 
