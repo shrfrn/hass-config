f3931131b251df4c86783c4a68a949e5788b1068
Sharon Frenkel <shrfrn@gmail.com>
2026-01-31 11:49:05 +0200

    Add icon to bedroom standing lamp

--- generator.config.js
+++ generator.config.js
@@ -50,7 +50,7 @@ const config = {
             syncedEntities: {
                 mb_standing_lamp: {
                     name: 'Master Bedroom Standing Lamp',
-					icon: 'mdi:floor-light-outline',
+					icon: 'mdi:floor-lamp-outline',
                     power: null, // Always powered - switch doesn't cut power to bulb
                     entities: [
                         { entity_id: 'switch.mb_soc', sync: true },
